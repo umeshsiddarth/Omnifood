2c2fd4c39a4d39d54373698b60091df15347b603
Umesh Siddarth <umeshsiddarth@gmail.com>
2024-03-09 20:28:18 +0530

    Add Smooth scroll effect using JavaScript
    Implementing scroll behaviour to smooth just using
    css doesn't work in safari. So we need to use JS
    to implement a smooth scroll behaviour along with
    polyfill script.

--- js/script.js
+++ js/script.js
@@ -8,8 +8,35 @@
 
 const hamMenu = document.querySelector(".btn-mobile-nav");
 const header = document.querySelector(".header");
+const allLinks = document.querySelectorAll("a:link");
 
 hamMenu.addEventListener("click", () => {
   header.classList.toggle("nav-open");
   console.log("Clicked");
+});
+
+// Smooth Scrolling
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+    // Scroll Back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    // Scroll to Other Links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close Mobile Menu
+    if (link.classList.contains("main-nav-link")) {
+      header.classList.toggle("nav-open");
+    }
+  });
 });
